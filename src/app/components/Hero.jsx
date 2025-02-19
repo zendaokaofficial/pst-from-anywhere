5066643cb27720c6dc5caccd834948287f7b74a4
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2025-02-19 14:15:02 +0800

    Update Hero.jsx
    Edit Form

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 
 const Hero = () => {
   const [keperluan, setKeperluan] = useState('');
+  const [jenisKunjungan, setJenisKunjungan] = useState('');
 
   const openModal = () => {
     document.getElementById('my_modal_4').showModal();
@@ -13,17 +14,19 @@ const Hero = () => {
     e.preventDefault();
   
     const formData = {
-      formType: 'BukuTamu', // Specify the form type
+      formType: 'BukuTamu',
       nama: e.target.nama.value,
       email: e.target.email.value,
       whatsapp: e.target.whatsapp.value,
-      keperluan, // Use state value
+      instansi: e.target.instansi.value,
+      keperluan,
+      jenisKunjungan,
       tanggal: e.target.tanggal.value,
       waktu: e.target.waktu.value,
     };
   
     try {
-      const response = await fetch('/api/sheet', { // Ensure this matches your backend API
+      const response = await fetch('/api/sheet', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -36,26 +39,21 @@ const Hero = () => {
       }
   
       const result = await response.json();
-      console.log(result); // Check if the response is correct
+      console.log(result);
       document.getElementById('my_modal_4').close();
       
-      // Reload the page after a successful submission
-      window.location.reload(); // This will refresh the page
+      window.location.reload();
   
     } catch (error) {
       console.error('Error submitting the form:', error);
     }
   };  
   
-
   return (
     <div>
       <div
         className="hero min-h-screen"
-        style={{
-          backgroundImage:
-            "url('/images/cover-hero-01.png')",
-        }}>
+        style={{ backgroundImage: "url('/images/cover-hero-01.png')" }}>
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-neutral-content text-center">
           <div className="max-w-md">
@@ -76,85 +74,48 @@ const Hero = () => {
           <h3 className="font-bold text-lg">Buku Tamu</h3>
           <form onSubmit={handleSubmit} className="py-4">
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">Nama</span>
-              </label>
-              <input
-                type="text"
-                name="nama"
-                className="input input-bordered"
-                required
-              />
+              <label className="label"><span className="label-text">Nama</span></label>
+              <input type="text" name="nama" className="input input-bordered" required />
             </div>
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                name="email"
-                className="input input-bordered"
-                required
-              />
+              <label className="label"><span className="label-text">Email</span></label>
+              <input type="email" name="email" className="input input-bordered" required />
             </div>
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">No. WhatsApp</span>
-              </label>
-              <input
-                type="text"
-                name="whatsapp"
-                className="input input-bordered"
-                required
-              />
+              <label className="label"><span className="label-text">No. WhatsApp</span></label>
+              <input type="text" name="whatsapp" className="input input-bordered" required />
             </div>
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">Keperluan</span>
-              </label>
-              <select
-                name="keperluan"
-                className="select select-bordered"
-                value={keperluan}
-                onChange={(e) => setKeperluan(e.target.value)}
-                required
-              >
-                <option value="" disabled>
-                  Pilih keperluan
-                </option>
+              <label className="label"><span className="label-text">Instansi/Institusi</span></label>
+              <input type="text" name="instansi" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label"><span className="label-text">Keperluan</span></label>
+              <select name="keperluan" className="select select-bordered" value={keperluan} onChange={(e) => setKeperluan(e.target.value)} required>
+                <option value="" disabled>Pilih keperluan</option>
                 <option value="Umum">Umum</option>
                 <option value="Pelayanan Statistik Terpadu (PST)">Pelayanan Statistik Terpadu (PST)</option>
               </select>
             </div>
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">Tanggal</span>
-              </label>
-              <input
-                type="date"
-                name="tanggal"
-                className="input input-bordered"
-                required
-              />
+              <label className="label"><span className="label-text">Jenis Kunjungan</span></label>
+              <select name="jenisKunjungan" className="select select-bordered" value={jenisKunjungan} onChange={(e) => setJenisKunjungan(e.target.value)} required>
+                <option value="" disabled>Pilih jenis kunjungan</option>
+                <option value="Kunjungan Langsung">Kunjungan Langsung</option>
+                <option value="Kunjungan Tidak Langsung (Online)">Kunjungan Tidak Langsung (Online)</option>
+              </select>
             </div>
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">Waktu</span>
-              </label>
-              <input
-                type="time"
-                name="waktu"
-                className="input input-bordered"
-                required
-              />
+              <label className="label"><span className="label-text">Tanggal</span></label>
+              <input type="date" name="tanggal" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label"><span className="label-text">Waktu</span></label>
+              <input type="time" name="waktu" className="input input-bordered" required />
             </div>
             <div className="modal-action">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-              <button type="button" className="btn" onClick={() => document.getElementById('my_modal_4').close()}>
-                Close
-              </button>
+              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="button" className="btn" onClick={() => document.getElementById('my_modal_4').close()}>Close</button>
             </div>
           </form>
         </div>
