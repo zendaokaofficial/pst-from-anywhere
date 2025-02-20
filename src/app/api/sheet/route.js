3a7053c0899f824e748b34b791fd975e0d1c911b
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2025-02-20 11:07:48 +0800

    Update route.js
    Update const private_key

--- src/app/api/sheet/route.js
+++ src/app/api/sheet/route.js
@@ -8,7 +8,7 @@ export async function POST(req) {
     const client = new google.auth.JWT(
       keys.client_email,
       null,
-      keys.private_key,
+      privateKey,
       ['https://www.googleapis.com/auth/spreadsheets']
     );
 
